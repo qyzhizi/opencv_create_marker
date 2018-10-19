1c2213f4a336b1505d6ad4f7459d2320912fcc56
qy <l2830942138@gmail.com>
2018-10-19 21:01:42 +0800

    first commit

---
+++ CMakeFiles/create_marker_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create_marker_exe.dir/create_marker.cpp.o"
+  "create_marker_exe.pdb"
+  "create_marker_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/create_marker_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
