95eb7e93a319cd603c7391ad992a70907fd4ffa7
qy <l2830942138@gmail.com>
2018-10-19 21:57:05 +0800

    update

--- create_marker.cpp
+++ create_marker.cpp
@@ -46,12 +46,12 @@ using namespace cv;
 namespace {
 const char* about = "Create an ArUco marker image";
 const char* keys  =
-        "{@outfile |out_0_20.jpg | Output image }"
-        "{d        |  0    | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
+        "{@outfile |out_2_5_200.jpg | Output image }"
+        "{d        |  2    | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
         "DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, "
         "DICT_6X6_50=8, DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12,"
         "DICT_7X7_100=13, DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16}"
-        "{id       |  20   | Marker id in the dictionary }"
+        "{id       |  5   | Marker id in the dictionary }"
         "{ms       | 200   | Marker size in pixels }"
         "{bb       | 1     | Number of bits in marker borders }"
         "{si       | 1 | show generated image }";
